760669491f8cbf696e879f74db27342e516e7a21
Rajat Kumar Jha <114743375+rajatdev1@users.noreply.github.com>
2023-05-12 14:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function valueButton(e){
+    calculatorform.screen.value += e.value;
+}
+
+function clear(){
+    calculatorform.screen.value = null; 
+}
+
+function calculate(){
+    calculatorform.screen.value =eval( calculatorform.screen.value );
+}
